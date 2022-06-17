88ddb45dac7c85bc446651e2ef05c828510566af
Rishikesh Kumar <anchalesh10@gmail.com>
2022-06-18 00:12:50 +0530

    kunal birthday present

--- effect.js
+++ effect.js
@@ -116,8 +116,8 @@ $("document").ready(function () {
 
   $("#balloons_flying").click(function () {
     $(".balloon-border").animate({ top: -500 }, 14000);
-    $("#b1,#b4,#b5,#b7").addClass("balloons-rotate-behaviour-one");
-    $("#b2,#b3,#b6").addClass("balloons-rotate-behaviour-two");
+    $("#b1,#b4,#b5,#b7,#b2,#b3,#b6").addClass("balloons-rotate-behaviour-one");
+    // $("#b2,#b3,#b6").addClass("balloons-rotate-behaviour-two");
     // $('#b3').addClass('balloons-rotate-behaviour-two');
     // $('#b4').addClass('balloons-rotate-behaviour-one');
     // $('#b5').addClass('balloons-rotate-behaviour-one');
